656c5dad398904dbbe51129a5eaa18119126f074
Jhoan <jpeguero1989@gmail.com>
2021-08-09 19:15:59 -0400

    route done

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
+import {Route} from 'react-router-dom'
+
 import './App.css';
+import Product from './routes/products';
+import Welcome from './routes/welcome';
 
 function App() {
   return (
     <div>
       <h1>GROUP-EATS</h1>
+      <Route path="/welcome">
+        <Welcome/>
+      </Route>
+
+      <Route path="/products">
+        <Product/>
+      </Route>
     </div>
   );
 }
